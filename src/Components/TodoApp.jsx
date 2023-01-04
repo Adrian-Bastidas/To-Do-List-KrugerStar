5216cbb2aaa45dfd97f2d04d3896a4ae8f699e63
magobasti <abastidas94@gmail.com>
2023-01-04 18:30:37 -0500

    Creación de la base para la to-do-list

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TodoApp = () => {
+    const [title, setTitle]=useState("Hola");
+    const [todos,settodos]=useState([]);
+
+    function handleChange(event){
+        const value= event.target.value;
+        setTitle(value)
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const newTodo={
+            id: crypto.randomUUID(),
+            title:title,
+            completed: false
+        }
+
+        const temp=[...todos];
+        temp.unshift(newTodo);
+        settodos(temp)
+    }
   return (
-    <div>TodoApp</div>
+    <div className='cointainer'>
+        <form className='createForm' onSubmit={handleSubmit}>
+            <input onChange={handleChange} className='tInput' value={title}/>
+            <input 
+            onClick={handleSubmit}
+            type="submit" 
+            value="Create todo" 
+            className='buttonCreate'/>
+        </form>
+        <div className='todosContainer'>
+            {
+                todos.map(item=>(
+                    <div key={item.id}>{item.title}</div>//Renderizado de elementos en lista de forma automatica
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
